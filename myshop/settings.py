3b0072060c3ea9f9cfb4884bb429ceb4d7a5deff
ioana <ioanav3796@gmail.com>
2019-10-13 19:16:19 +0200

    changed settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'i#=i*ufnrnv0rbds&b)e2l@e1g#p&+y!ku$ewv9h#l)!+i1yk3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = *
+ALLOWED_HOSTS = []
 
 
 # Application definition
